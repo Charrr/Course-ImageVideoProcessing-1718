d1ea41ecec367eb57111d726492692967a810533
Charrr <906355844@qq.com>
2017-12-05 00:19:31 +0800

    writing functions
    ppmwrite.m created,
    pgmwrite.m modified correspondingly

--- ppmwrite.m
+++ ppmwrite.m
@@ -1,47 +1,53 @@
-function pgmwrite(I,l,mode)
-%PGMWRITE Writes a data matrix into a pgm file.
+function ppmwrite(I,l,mode)
+%PPMWRITE Writes a data matrix into a ppm file.
 % Input Parameters:
-%                I: 2-dimensional data matrix (w*h)
-%                l: grey levels
+%                I: 3-dimenstional data matrix (w*h*3)
+%                l: colour levels
 %             mode: ASCII or Binary (1 for Ascii and 0 for Binary)
 
-[w,h] = size(I);    % size of the image is the dimension of the matrix
+[m,n] = size(I(:,:,1));    % dimension of the matrix
+w = m/3;            % 3 values per pixel (R,G,B)
+h = n;
 
 % open the file in read mode
 if (mode == 1) ; % Ascii mode
-    f = fopen('outascii.pgm','w');
-    fprintf(f,'P2\n');
+    f = fopen('outascii.ppm','w');
+    fprintf(f,'P3\n');
     fprintf(f,'#Image created by Ziqian Chen\n');   % Signature.
     fprintf(f,'\n# image width\n');
     fprintf(f,'%i',w);
     fprintf(f,'\n# image height\n');
     fprintf(f,'%i',h);
-    fprintf(f,'\n# greyscale depth\n');
+    fprintf(f,'\n# colour depth\n');
     fprintf(f,'%i',l);
     fprintf(f,'\n');
     
     for i=1:w
         for j=1:h
-            fprintf(f,' %s ',num2str(I(i,j)));
+            fprintf(f,' %s ',num2str(I(i,j,1))); % R
+            fprintf(f,' %s ',num2str(I(i,j,2))); % G
+            fprintf(f,' %s ',num2str(I(i,j,3))); % B
             fprintf(f,'\n');
         end
     end
     
 else            % Binary mode
-    f = fopen('outbinary.pgm','w');
-    fprintf(f,'P5\n');
+    f = fopen('outbinary.ppm','w');
+    fprintf(f,'P6\n');
     fprintf(f,'#Image created by Ziqian Chen\n');   % Signature.
     fprintf(f,'\n# image width\n');
     fprintf(f,'%i',w);
     fprintf(f,'\n# image height\n');
     fprintf(f,'%i',h);
-    fprintf(f,'\n# greyscale depth\n');
+    fprintf(f,'\n# colour depth\n');
     fprintf(f,'%i',l);
     fprintf(f,'\n');
     
     for i=1:w
         for j=1:h
-            fprintf(f,'%c',I(i,j));
+            fprintf(f,'%c',I(i,j,1)); % R
+            fprintf(f,'%c',I(i,j,2)); % G
+            fprintf(f,'%c',I(i,j,3)); % B
         end
     end
     
