04470951bec2904d8e381a72eac93b0d66d49727
Charrr <906355844@qq.com>
2017-12-12 19:40:45 +0800

    Well I did a test on inverse YUV conversion
    for   fun ??? anyway

--- ex_commands.m
+++ ex_commands.m
@@ -3,8 +3,8 @@
 % 1b
 [LENA_RGB, l] = ppmread('img2014/Lena512C_ASCII2014.ppm');
 LENA_YUV = BUPT_format_converter(LENA_RGB);
-pgmwrite(LENA_YUV(:,:,1), l, 1, '1b_LENA');
+% pgmwrite(LENA_YUV(:,:,1), l, 1, '1b_LENA');
 
 [BABOON_RGB, l] = ppmread('img2014/Baboon512C_Binary2014.ppm');
 BABOON_YUV = BUPT_format_converter(BABOON_RGB);
-pgmwrite(BABOON_YUV(:,:,1), l, 0, '1b_BABOON');
+% pgmwrite(BABOON_YUV(:,:,1), l, 0, '1b_BABOON');
